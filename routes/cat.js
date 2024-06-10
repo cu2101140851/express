c297bebd88f4e2222260370b77a870f7a37b94a6
cu2101140851 <2101140851cb@cyber-u.ac.jp>
2024-06-11 08:12:49 +0900

    add cat api6

--- routes/cat.js
+++ routes/cat.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+
 router.get('/', async (req, res) => {
 request('https://api.thecatapi.com/v1/images/search', function (error, response, 
 body) {
@@ -10,6 +11,7 @@ res.json(data);
 }
 });
 })
+
 module.exports = router;
 
 
